870d275f20ed9a383b65fbed95735c4e8e266a83
hitcode47 <bruno29@outlook.com.br>
2024-01-27 23:43:56 -0300

    Alterar aluramidi-curso, melhoria nas funcionalidades da mesa de sons

--- JS_aluramidi-curso-arquivos-iniciais/index.js
+++ JS_aluramidi-curso-arquivos-iniciais/index.js
@@ -2,16 +2,16 @@ function tocaSom(id_som_tecla){
     // Função que busca a referência através de um id para as teclas de som e executa o som solicitado
     const teclaRequisitada = document.querySelector(id_som_tecla);
     if (teclaRequisitada == undefined || teclaRequisitada == null) {
-        alert('Função não encontrada')
+        alert('Tecla incorreta');
     }
     else{
         teclaRequisitada.play();
     }
 }
-    // lista que recebe referência para todos os elementos com a classe tecla dentro do html
+// lista que recebe referência para todos os elementos com a classe tecla dentro do html
 const listadeteclas = document.querySelectorAll('.tecla'); 
 
-const mapeamentoDeBotoes = {
+const mapeamentoAudio = {
     "q" : "#som_tecla_pom",
     "w" : "#som_tecla_clap",
     "e" : "#som_tecla_tim",
@@ -20,7 +20,19 @@ const mapeamentoDeBotoes = {
     "d" : "#som_tecla_toim",
     "z" : "#som_tecla_psh",
     "x" : "#som_tecla_tic",
-    "c" : "#som_tecla_tom"
+    "c" : "#som_tecla_tom",
+
+}
+const mapeamentoBotoes = {
+    "q" : ".tecla_pom",
+    "w" : ".tecla_clap",
+    "e" : ".tecla_tim",
+    "a" : ".tecla_puff",
+    "s" : ".tecla_splash",
+    "d" : ".tecla_toim",
+    "z" : ".tecla_psh",
+    "x" : ".tecla_tic",
+    "c" : ".tecla_tom"
 }
 for (let contador = 0; contador < listadeteclas.length; contador++){ 
     // laço para varrer todos os elementos da bandeja de sons e atribuir um evento onclick a cada elemento
@@ -31,18 +43,25 @@ for (let contador = 0; contador < listadeteclas.length; contador++){
     
     tecla.onclick = function(){
         tocaSom(nome_audio);
+        tecla.classList.add('ativa');
+        tecla.classList.remove('ativa');
     }
+
     tecla.onkeydown = function(elemento){
-        
-        tocaSom(mapeamentoDeBotoes[elemento.key]);
-        if(elemento.code === 'Space' || elemento.doce === 'Enter' || elemento.key == elemento.key){
-        tecla.classList.add("ativa");
+        if(elemento.key in mapeamentoAudio){
+            tocaSom(mapeamentoAudio[elemento.key]);
+            var bot = document.querySelector(mapeamentoBotoes[elemento.key]);
+            bot.classList.add("ativa");
+            bot.focus();
         }
-        else{
-            console.log(elemento.code);
+        else if(elemento.key == 'Tab'){
+            console.log(tecla.classList[1]);
         }
     }
-    tecla.onkeyup = function(){
-        tecla.classList.remove("ativa");
+    tecla.onkeyup = function(elemento){
+        if (elemento.key in mapeamentoAudio) {
+            var bot = document.querySelector(mapeamentoBotoes[elemento.key]);
+            bot.classList.remove("ativa");
+        }
     }
 }
